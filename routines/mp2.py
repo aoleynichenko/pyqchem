80fa9ae1205e2677a0ff4ba5a150243533f42e82
aoleynichenko <ao2310@yandex.ru>
2016-04-30 22:22:44 +0300

    bug fix in MP3

--- routines/mp2.py
+++ routines/mp2.py
@@ -34,21 +34,21 @@ def mp3_energy(dim,Nelec,ints,E):
                 for a in range(Nelec,dim*2):
                     for b in range(Nelec,dim*2):
                         for c in range(Nelec,dim*2):
-                            diag1 -= ints[i,k,a,c]*ints[a,j,b,k]*ints[b,c,i,j]/(E[i//2]+E[k//2]-E[a//2]-E[c//2])*(E[i//2]+E[j//2]-E[b//2]-E[c//2])
+                            diag1 -= ints[i,k,a,c]*ints[a,j,b,k]*ints[b,c,i,j]/((E[i//2]+E[k//2]-E[a//2]-E[c//2])*(E[i//2]+E[j//2]-E[b//2]-E[c//2]))
     for i in range(0,Nelec):
         for j in range(0, Nelec):
             for a in range(Nelec,dim*2):
                 for b in range(Nelec,dim*2):
                     for c in range(Nelec,dim*2):
                         for d in range(Nelec,dim*2):
-                            diag2 += 0.125*ints[i,j,a,c]*ints[a,c,b,d]*ints[b,d,i,j]/(E[i//2]+E[j//2]-E[a//2]-E[c//2])*(E[i//2]+E[j//2]-E[b//2]-E[d//2])
+                            diag2 += 0.125*ints[i,j,a,c]*ints[a,c,b,d]*ints[b,d,i,j]/((E[i//2]+E[j//2]-E[a//2]-E[c//2])*(E[i//2]+E[j//2]-E[b//2]-E[d//2]))
     for i in range(0,Nelec):
         for j in range(0, Nelec):
             for k in range(0, Nelec):
                 for l in range(0, Nelec):
                     for a in range(Nelec,dim*2):
                         for b in range(Nelec,dim*2):
-                            diag3 += 0.125*ints[i,j,a,b]*ints[k,l,i,j]*ints[a,b,k,l]/(E[i//2]+E[j//2]-E[a//2]-E[b//2])*(E[k//2]+E[l//2]-E[a//2]-E[b//2])
+                            diag3 += 0.125*ints[i,j,a,b]*ints[k,l,i,j]*ints[a,b,k,l]/((E[i//2]+E[j//2]-E[a//2]-E[b//2])*(E[k//2]+E[l//2]-E[a//2]-E[b//2]))
     return diag1 + diag2 + diag3
 
 
